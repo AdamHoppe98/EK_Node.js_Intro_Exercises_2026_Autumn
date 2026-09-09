3306b3c41172d3c47aabe8b4835ea4cff53bf69c
AdamHoppe98 <adamhoppe98@gmail.com>
2026-09-09 10:04:39 +0200

    fix

--- 02._Animals_Exercise/app.js
+++ 02._Animals_Exercise/app.js
@@ -2,24 +2,26 @@ const express = require('express');
 
 const app = express();
 
-const jaguar = {name: "Jaguar", id: 1}
-const lion = {name: "Lion", id: 2}
-const Elephant = {name: "Elephant", id: 3}
+const jaguar = { name: "Jaguar", id: 1 }
+const lion = { name: "Lion", id: 2 }
+const Elephant = { name: "Elephant", id: 3 }
 
 const animals = [
     jaguar, lion, Elephant
 ]
 
 app.get('/animals', (req, res) => {
-    res.send({data: animals});
+    res.send({ data: animals });
 })
 
 app.get('/animals/:id', (req, res) => {
-    const id = Number(req.params.id);
-    for (let i = 0; i < animals.length; i++ ){
-        if (animals[i].id === id)
-            res.send({data: animals[i]})
+    const animal = animals.find(e => e.id === Number(req.params.id));
+
+    if (!animal){
+        return res.send({data: "no animal have that id"})
     }
+
+    res.send({ data: animal })
 })
 
 app.listen(8080)
