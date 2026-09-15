e86180bf7c50c7ab1074ebff1004b0874a8a2b29
AdamHoppe98 <adamhoppe98@gmail.com>
2026-09-15 09:31:18 +0200

    animals done

--- 02._Animals_Exercise/app.js
+++ 02._Animals_Exercise/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.json())
 const jaguar = { name: "Jaguar", id: 1 }
 const lion = { name: "Lion", id: 2 }
 const Elephant = { name: "Elephant", id: 3 }
@@ -15,13 +15,35 @@ app.get('/animals', (req, res) => {
 })
 
 app.get('/animals/:id', (req, res) => {
-    const animal = animals.find(e => e.id === Number(req.params.id));
+    const animal = animals.find(e => e.id === parseInt(req.params.id));
 
     if (!animal){
         return res.send({data: "no animal have that id"})
     }
-
     res.send({ data: animal })
 })
+
+app.post('/animals', (req, res) => {
+    const newAnimal = { id: animals.length + 1, name: req.body.name }
+    animals.push(newAnimal);
+    res.status(201).json(newAnimal);
+})
+
+app.put('/animals/:id', (req, res) => {
+   const providedId = Number(req.params.id);
+   const foundIndex = animals.findIndex(item => item.id === providedId);
+
+   if (foundIndex === -1){
+    return res.status(404).json({ data: `No animal found by id: ${providedId}` })
+   }
+   animals.splice(foundIndex, 1);
+   const newID = 
+   const updatedAnimal = { name: req.body.name, id: animals.length + 1 };
+
+   animals.push(updatedAnimal);
+   res.send(updatedAnimal);
+});
+
+
 
 app.listen(8080)
